408d50ea11e73db56ebca15ca787048f07d625b4
Rocio <silvana.blaq@gmail.com>
2022-06-13 21:08:50 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const nombre = 'Rocio';
+const apellido = 'Zalazar';
+
+//const nombreCompleto = nombre + ' ' + apellido;
+//const nombreCompleto =  `Hola Mundo`;
+const nombreCompleto = `${nombre} ${apellido}`;
+
+console.log(nombreCompleto);
+
+
+function getSaludo() {
+    return 'Hola mondo';
+}
+console.log(`Este es un texto: ${getSaludo()}`);
