4949652cfb70ffe99235fe6c9de5b8291ef116fa
Rocio <silvana.blaq@gmail.com>
2022-06-15 16:48:13 -0300

    index y arrays.js
    Prueba y limpieza en idex, luego se pasa al archivos de Arrays.js

---
+++ 01_intro_react/04-arrays.js
@@ -0,0 +1,20 @@
+//arreglos en JS
+
+//const arreglo = new Array(100);
+const arreglo = [1, 2, 3, 4];
+
+/* arreglo.push(1);
+arreglo.push(2);
+arreglo.push(3);
+arreglo.push(4); */
+
+let arreglo2 = [...arreglo, 5];
+//arreglo2.push(5);
+
+const arreglo3 = arreglo2.map(function name(numero) {
+    return numero * 5
+});
+
+console.log(arreglo);
+console.log(arreglo2);
+console.log(arreglo3)
--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-//arreglos en JS
 
-//const arreglo = new Array(100);
-const arreglo = [1, 2, 3, 4];
 
-/* arreglo.push(1);
-arreglo.push(2);
-arreglo.push(3);
-arreglo.push(4); */
 
-let arreglo2 = [...arreglo, 5];
-//arreglo2.push(5);
-
-const arreglo3 = arreglo2.map();
-
-console.log(arreglo);
-HTMLFormControlsCollection.log(aareglo2)
